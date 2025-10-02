af35c6d01b7de6e08845a15af8d07b60b2bd93ca
joaoferreira92-dev <>
2025-10-02 10:33:33 +0100

    Digital Cluster App
    Digital Cluster App with small
    adaptations for Jaccus

---
+++ digital-cluster-app/app/src/main/java/com/example/digitalclusterapp/core/data/repository/MqttClusterRepository.kt
@@ -0,0 +1,15 @@
+package com.example.digitalclusterapp.core.data.repository
+
+import com.example.digitalclusterapp.core.domain.model.ClusterState
+import kotlinx.coroutines.flow.StateFlow
+
+interface MqttClusterRepository {
+    val state: StateFlow<ClusterState>
+
+    suspend fun updateSpeed(speed: Int): Boolean
+    suspend fun updateRpm(rpm: Int): Boolean
+    suspend fun toggleCruiseControl(): Boolean
+    suspend fun updateCruiseControlSpeed(speed: Int): Boolean
+    suspend fun toggleLocation(): Boolean
+    suspend fun updateClusterState(newState: ClusterState): Boolean
+}
